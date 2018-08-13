cbde457ef0f658c818a0ef173692f9b3d7b86889
Chiara Cokieng <chiara@chiaracokieng.com>
2018-08-13 06:43:40 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,6 +3,7 @@ var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 function addElementToBeginningOfArray(array, element) {
   var friends = ['Gella', 'Katlee']
   friends.unshift = ['Nikki']
+  return friends
 }
 
 function destructivelyAddElementToBeginningOfArray() {
